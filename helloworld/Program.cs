7b8ef5d5c5fafcdf50a2c1ac7fe877839584f578
Anwardeen Maideen Pichai <kalilanwar597@gmail.com>
2025-10-14 23:43:30 +0530

    learning about the strings

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Anwar!");
+
+string firstFriend = "shaheel";
+string secondFriend = "Sayed";
+string thirdFriend = "Khaled";
+Console.WriteLine($"My friends are: {firstFriend}, {secondFriend}, and {thirdFriend}.");
+
+
