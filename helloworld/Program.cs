7c6cf342235c3e4fe9e305565320376b464c871f
Anwardeen Maideen Pichai <kalilanwar597@gmail.com>
2025-10-16 23:57:48 +0530

    Learning about strings like trim,removing spaces

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,9 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Anwar!");
 
-string firstFriend = "shaheel";
-string secondFriend = "Sayed";
-string thirdFriend = "Khaled";
-Console.WriteLine($"My friends are: {firstFriend}, {secondFriend}, and {thirdFriend}.");
+string firstFriend = "  shaheel    ";
+firstFriend = firstFriend.TrimStart();// trims the white spaces from the start of the string
+
+string secondFriend = "   Sayed   ";
+
+string thirdFriend = "Khaled   ";
+thirdFriend = thirdFriend.TrimEnd();// trims the white spaces from the end of the string    
+
+
+Console.WriteLine($"My friends are: {firstFriend}, {secondFriend.Trim()}, and {thirdFriend}.");
 
 
