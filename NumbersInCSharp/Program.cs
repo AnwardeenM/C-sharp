f830a0324e3f2def91f0e561c637e61e9de494f7
Anwardeen Maideen Pichai <kalilanwar597@gmail.com>
2025-11-20 16:25:43 +0530

    Adding some reference code

--- NumbersInCSharp/Program.cs
+++ NumbersInCSharp/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 18;
+int b =6;
+int c = a + b;
+Console.WriteLine("The sum of a and b is: " + c);
