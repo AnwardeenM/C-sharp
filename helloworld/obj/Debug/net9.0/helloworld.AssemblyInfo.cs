01cd2f5e4914ec22be7cd56d49b96e3f457e1f48
Anwardeen Maideen Pichai <kalilanwar597@gmail.com>
2025-11-20 15:27:44 +0530

    Updated the .gitignore file to remove the build files.

--- helloworld/obj/Debug/net9.0/helloworld.AssemblyInfo.cs
+++ helloworld/obj/Debug/net9.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7dcb2e20683fa07255c254640c82ff38e6578b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e14adf29fd46eadfa0bd837ca929333f5c6b779")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
